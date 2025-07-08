fe3440553e8bc0a01cc85eb45c0d4fd517f2e4c1
Raúl S. <98350729+raulscarvalho@users.noreply.github.com>
2025-07-08 10:46:30 -0300

    atualizacao no upload de foto na tela de cadastro

--- routes/cadastro.js
+++ routes/cadastro.js
@@ -2,31 +2,17 @@ const express = require('express');
 const router = express.Router();
 const Usuario = require('../models/Usuario');
 const multer = require('multer');
-const path = require('path');
+const bcrypt = require('bcrypt');
 
-// Configurar upload das ftos de perfil
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'public/uploads/');
-  },
-  filename: (req, file, cb) => {
-    const ext = path.extname(file.originalname);
-    const nomeArquivo = `${Date.now()}-${file.fieldname}${ext}`;
-    cb(null, nomeArquivo);
-  }
-});
-
+const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
-// Página de cadastro
 router.get('/', (req, res) => {
   res.render('cadastro', { erro: null });
 });
 
-// Cadastro com foto
 router.post('/', upload.single('foto'), async (req, res) => {
   const { nome, email, senha, curso } = req.body;
-  const foto = req.file ? `/uploads/${req.file.filename}` : null;
 
   try {
     const jaExiste = await Usuario.findOne({ email });
@@ -34,9 +20,23 @@ router.post('/', upload.single('foto'), async (req, res) => {
       return res.render('cadastro', { erro: 'E-mail já cadastrado' });
     }
 
-    const novoUsuario = new Usuario({ nome, email, senha, curso, foto });
-    await novoUsuario.save();
+    const senhaHash = await bcrypt.hash(senha, 10);
 
+    const novoUsuario = new Usuario({
+      nome,
+      email,
+      senha: senhaHash,
+      curso
+    });
+
+    if (req.file) {
+      novoUsuario.foto = {
+        data: req.file.buffer,
+        contentType: req.file.mimetype
+      };
+    }
+
+    await novoUsuario.save();
     req.session.usuario = novoUsuario;
     res.redirect('/');
   } catch (err) {
